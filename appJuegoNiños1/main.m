711d3432bd428af39777dd8b7ef3fc9ce2139667
betoFlores58 <albertfr@live.com.mx>
2020-10-27 19:50:17 -0600

    Initial Commit

---
+++ appJuegoNiños1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  appJuegoNiños1
+//
+//  Created by Alberto Flores on 27/10/20.
+//  Copyright © 2020 Alberto Flores. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
